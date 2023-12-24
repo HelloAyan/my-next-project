9ae6e144dfa9f6ce80374a6c0caf0bcbcc6601d7
HelloAyan <18103373@iubat.edu>
2023-12-24 10:28:02 +0600

    Project transfered from local to github

---
+++ src/app/about/page.js
@@ -0,0 +1,8 @@
+function about() {
+    return (
+        <>
+            <h1>About page</h1>
+        </>
+    )
+}
+export default about;
---
+++ src/app/contact/page.js
@@ -0,0 +1,8 @@
+function contact() {
+    return (
+        <>
+            <h1>Contact Page</h1>
+        </>
+    )
+}
+export default contact;
